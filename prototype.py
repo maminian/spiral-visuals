bbde1f42f7654e34f36030182ab22171c01f1572
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2019-11-27 16:27:35 -0700

    another optimization

--- prototype.py
+++ prototype.py
@@ -5,7 +5,13 @@ fig2,ax2 = pyplot.subplots(1,1, figsize=(6,6))
 ax2.set_facecolor('k')
 
 def get_arc(rs,ths, narc=16):
-    ss = np.linspace(0,1,narc)
+    if ths[0]==ths[1]:
+        # purely radial movement; only need two points.
+        ss = np.linspace(0,1,2)
+    else:
+        ss = np.linspace(0,1,narc)
+    #
+
     xvs = (rs[0] + ss*(rs[1]-rs[0]))*np.cos(ths[0] + ss*(ths[1]-ths[0]))
     yvs = (rs[0] + ss*(rs[1]-rs[0]))*np.sin(ths[0] + ss*(ths[1]-ths[0]))
     return xvs,yvs
